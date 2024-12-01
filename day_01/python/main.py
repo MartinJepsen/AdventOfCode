1dd107c036e5df91b31d018a36ee5a6d8e00b7ca
Martin Jepsen <majp@ramboll.com>
2024-12-01 18:56:04 +0100

    [python] solve day 1, part 2

--- day_01/python/main.py
+++ day_01/python/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent.parent / "input.txt"
 
-
+# Part 1
 def get_lists() -> tuple[list[int], list[int]]:
     """Get the two lists of numbers from the input file, sorted in ascending order."""
     list_left: list[int] = []
@@ -22,12 +22,24 @@ def get_difference(list_left: list[int], list_right: list[int]) -> int:
     """Get the sum element-wise difference between the two lists."""
     return sum(abs(left - right) for left, right in zip(list_left, list_right))
 
+# Part 2
+def get_similarity_score(list_left: list[int], list_right: list[int]) -> int:
+    """Get the similarity score between the two lists."""
+    similarity: int = 0
+    for number in set(list_left):
+        similarity += number * list_right.count(number)
+    return similarity
+
 
 def main():
     list_left, list_right = get_lists()
     difference = get_difference(list_left, list_right)
 
     print(f"The sum of the differences between the two lists is: {difference}")
+
+    # Part 2
+    similarity_score = get_similarity_score(list_left, list_right)
+    print(f"The similarity score between the two lists is: {similarity_score}")
 
 
 
